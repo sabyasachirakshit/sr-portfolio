b1caf1adbc769d4a8bc70c0370074ce0c081f6e9
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-10 11:50:23 +0530

    navbar elements added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div
+      className="sr-navbar"
+      style={{
+        backgroundColor: "red",
+        display: "flex",
+        padding: 40,
+        justifyContent: "space-between",
+      }}
+    >
+      <div className="srn-1" style={{ paddingLeft: 60 }}>
+        <b style={{ fontSize: 50 }}>SR</b>
+      </div>
+      <div
+        className="srn-2"
+        style={{
+          display: "flex",
+          gap: 28,
+          paddingRight: 100,
+          fontSize: 25,
+          position: "relative",
+          top: 14,
+        }}
+      >
+        <span>about</span>
+        <span>contact</span>
+        <span>work</span>
+        <span>playground</span>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
