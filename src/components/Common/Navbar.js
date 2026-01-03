10ecdd3afcd371d63b18773d32d7c1abd742b6e9
Sabyasachi <sabyasachirakshitacs19@gmail.com>
2026-01-03 13:09:47 +0530

    refactored Navbar component layout and styles for improved design

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -3,10 +3,23 @@ import "../../styles/Navbar/navbar.css"
 
 function Navbar() {
   return (
-    <div className="navbar" style={{display:"flex",justifyContent:"space-between"}}>
-      <div>Portfolio</div>
-
-      <div className="links" style={{display:"flex",justifyContent:"space-evenly",width:"30%"}}>
+    <div className="navbar" style={{ display: "flex", justifyContent: "space-between", backgroundColor: "white", height: "12vh" }}>
+      <div
+        className="heading1"
+        style={{
+          padding: "1rem",
+          fontSize: 24,
+          marginLeft: "1rem",
+          color: "transparent",
+          fontWeight: "700",
+          color: "#4f46e5",
+          letterSpacing: "0.5px",
+          display: "inline-block"
+        }}
+      >
+        Portfolio
+      </div>
+      <div className="links" style={{ display: "flex", justifyContent: "space-evenly", width: "30%", padding: "1rem" }}>
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/about">About</Link>
