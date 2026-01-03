8fc256cc4c75f90f9bfd7e423d7f5b35acde32a3
Sabyasachi <sabyasachirakshitacs19@gmail.com>
2026-01-03 10:35:03 +0530

    created basic components and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./containers/Home";
+import Projects from "./containers/Projects";
+import About from "./containers/About";
+import Contact from "./containers/Contact";
+
+import Navbar from "./components/Common/Navbar";
+import Footer from "./components/Common/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello I'm sabyasachi!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </Router>
   );
 }
 
