67871be51109af94c664e1dee2cb1eb3176c1f35
Sabyasachi <sabyasachirakshitacs19@gmail.com>
2026-01-05 10:51:59 +0530

    refactor App component to remove routing and simplify structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Home from "./containers/Home";
-import Projects from "./containers/Projects";
-import About from "./containers/About";
-import Contact from "./containers/Contact";
-
 import Navbar from "./components/Common/Navbar";
 import Footer from "./components/Common/Footer";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-
+      <Home />
       <Footer />
-    </Router>
+    </>
   );
 }
 
