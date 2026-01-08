7ca6d7d6348908f8cfd35ed4abb8d57a4d991bf3
Sabyasachi <sabyasachirakshitacs19@gmail.com>
2026-01-08 18:29:10 +0530

    feat: enhance About component layout with header and styling

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div id="about" className="about">
-        <h2>About</h2>
+    <div id="about" className="about" style={{marginTop:"1rem"}}>
+      <div className="about-me-header">
+        <div style={{fontSize:"40px"}}><b>About Me</b></div>
+        <div className="hor-line" style={{height:"5px",width:"20%",backgroundColor:"red"}}></div>
       </div>
-  )
+    </div>
+  );
 }
