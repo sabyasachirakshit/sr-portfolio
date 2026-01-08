e1f7950709bad8cd7f391a6e002f21c5c7327151
Sabyasachi <sabyasachirakshitacs19@gmail.com>
2026-01-08 18:37:55 +0530

    feat: update horizontal line width in About component for better visual alignment

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,11 +1,11 @@
-import React from "react";
+import "../styles/About/about.css"
 
 export default function About() {
   return (
     <div id="about" className="about" style={{marginTop:"1rem"}}>
       <div className="about-me-header">
         <div style={{fontSize:"40px"}}><b>About Me</b></div>
-        <div className="hor-line" style={{height:"5px",width:"20%",backgroundColor:"red"}}></div>
+        <div className="hor-line" style={{height:"5px",width:"9%",backgroundColor:"red"}}></div>
       </div>
     </div>
   );
